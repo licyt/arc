407c968c759900bfc6f61975f8d0bc93d734e0e9
tomcat <tomcat@3e5cd8aa-48b7-d44e-8ddf-11305824200f>
2016-04-11 13:43:54 +0000

    cCssHideTags implements tag hidding for LABEL and INPUT Tags that are autoincrementing
    git-svn-id: file:///var/vcs/repos/svn@64 3e5cd8aa-48b7-d44e-8ddf-11305824200f

--- cssHideIdTags.php
+++ cssHideIdTags.php
@@ -1,47 +1,53 @@
-//
-// This script generates css file that implements tag hidding for idLabelTag and idInputTag
-//
-// USAGE: Run in browser and copy+paste the output into desired css file
-//...
-
-include_once ("./database.php");
-include_once("./dbConfig.php");
-
 <?php
-	echo "Begin"
-	
-	$db = new cDbScheme;
-		
-	$db->link($dbServerName, $dbUser, $dbPassword);
-	
-	// get all table names
-	$db->useDb($dbName);
 
-	// for each table name
-	foreach ($db->tables[$tableName] as $i => $value) {
-        // create an instance of cDbTable
-		if(!empty ($table) ) {
-			unset($table);
-		}
-		$table = new cDbTable($value);
-		
-		// find which field is autoincrement
-		foreach ($table->fields as $j => $field ) {
-			//
-			if ( !($field->isAutoInc()) ) {
-				continue;
-			}
-			else {
-				// we found our autoincrementing field
-				echo $field->getFieldByName();
-				break;
+//
+// cCssHideTags implements tag hidding for LABEL and INPUT Tags that are autoincrementing
+//
+
+include_once ( "./database.php" );
+include_once ( "./cfile.php" );
+
+interface iCssHideIdTags
+{
+	public function __construct( $dbServerName, $dbUser, $dbPassword, $dbName , $cssFilePath , $cssFileName );
+	public function createCssFile();
+}
+
+class cCssHideIdTags extends cDbScheme implements iCssHideIdTags
+{	
+	protected $cssFileName = "";
+	protected $fileInstance = "";
+	
+	public function __construct( $dbServerName = "", $dbUser = "", $dbPassword = "", $dbName = "" , $cssFilePath = "" , $cssFileName = "" ) {
+		$this->dbServerName = $dbServerName;
+		$this->dbUser = $dbUser;
+		$this->dbPassword = $dbPassword;
+		$this->dbName = $dbName;
+		$this->cssFileName = $cssFileName;
+		$this->cssFile = new cFile( $cssFilePath, $cssFileName , "t" );
+		$this->cssFile->fileTruncate();
+	}
+
+	public function createCssFile() {
+		$this->cssFile->setMod('a');
+		$this->link( $this->dbServerName, $this->dbUser, $this->dbPassword );
+		$this->useDb( $this->dbName );
+		foreach ( $this->tables as $tableName => $table ) {
+			foreach ( $table->fields as $i => $field ) {
+				if ( !( $field->isAutoInc() ) ) {
+					// this field is not autoincrementing
+					continue;
+				}
+				else {
+					// we got an autoincrementing field
+					$string = "#".$field->getName()." {"."\r\n   display: none;"."\r\n"."}"."\r\n"."\r\n";
+					$string .= "#Label".$field->getName()." {"."\r\n   display: none;"."\r\n"."}"."\r\n"."\r\n";
+					// echo $string;
+					$this->cssFile->fileAppend( $string );
+					break;
+				}
 			}
 		}
 	}
-	
-	// missing disconnect
-	
-	echo "End"
-// cssIdTagScript
-
+}
 ?>
