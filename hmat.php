196e7cd39b4ebe756e1ab3686b7eacc4ab53b7f3
tomcat <tomcat@3e5cd8aa-48b7-d44e-8ddf-11305824200f>
2016-04-29 16:01:37 +0000

    Include of new css files into html header.
    git-svn-id: file:///var/vcs/repos/svn@124 3e5cd8aa-48b7-d44e-8ddf-11305824200f

--- hmat.php
+++ hmat.php
@@ -9,11 +9,27 @@ echo
     charset().
 	linkCss("css/hmat.css").
 	linkCss("css/hideIdTags.css").
-  	linkCss("css/tabHeadAdminInput.css").
-  	linkCss("css/tabHeadAdminSpan.css").
-  	linkCss("css/switchBrowserInput.css").
-  	linkCss("css/switchBrowserSpan.css").
-  	linkCss("css/switchBrowseForm.css").
+  	linkCss("css/project.css").
+  	linkCss("css/quote.css").
+  	linkCss("css/status.css").
+  	linkCss("css/note.css").
+  	linkCss("css/dataset.css").
+  	linkCss("css/demand.css").
+  	linkCss("css/invoice.css").
+  	linkCss("css/build.css").
+  	linkCss("css/deliverynote.css").
+  	linkCss("css/industry.css").
+  	linkCss("css/company.css").
+  	linkCss("css/account.css").
+  	linkCss("css/address.css").
+  	linkCss("css/person.css").
+  	linkCss("css/printparameters.css").
+  	linkCss("css/part.css").
+  	linkCss("css/platform_has_part.css").
+  	linkCss("css/platform.css").
+  	linkCss("css/deliverytransport.css").
+  	linkCss("css/gui.css").
+  	linkCss("css/statuslog.css").
 	linkCss("css/datepicker.css").
   	linkJs("js/jscolor.js").
 	linkJs("js/datepicker.js")
