7e860b5bee727599295c585b9a18a91d3b8152d6
tomcat <tomcat@3e5cd8aa-48b7-d44e-8ddf-11305824200f>
2016-05-09 14:41:43 +0000

    Added new ajax user as a role for dblogin for ajax scripts.
    git-svn-id: file:///var/vcs/repos/svn@156 3e5cd8aa-48b7-d44e-8ddf-11305824200f

--- dbConfig.php
+++ dbConfig.php
@@ -2,6 +2,8 @@
   $dbServerName = 'wendelstein';
   $dbUser       = 'root';
   $dbPassword   = 'mindfold';
+  $dbAjaxUser 	= 'root';
+  $dbAjaxPassword = 'mindfold';
   $dbName       = 'hmat_dev';
   
   $lang 		= 'ENG';
