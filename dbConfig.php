ace68426094ad294d78b6a171683eadb6f4ac63a
tomcat <tomcat@3e5cd8aa-48b7-d44e-8ddf-11305824200f>
2016-06-15 16:33:44 +0000

    Host by name to avoid domain name lookups.
    git-svn-id: file:///var/vcs/repos/svn@229 3e5cd8aa-48b7-d44e-8ddf-11305824200f

--- dbConfig.php
+++ dbConfig.php
@@ -1,6 +1,6 @@
 <?php
 // 2016 (C) Patrick SiR El Khatim, zayko5@gmail.com 
-$dbServerName = 'tokamag';
+$dbServerName = gethostbyname('tokamag.nasa.lan'); //localy resolves domain name and uses corresponding IP
   $dbName       = 'hmat_dev';
   $dbUser       = 'root';
   $dbPassword   = 'mindfold';
